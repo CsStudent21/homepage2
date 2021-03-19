b9b35ef288754016bf0a68cdcf8bdbbad6243c6c
CsStudent21 <tpadula@hey.com>
2021-03-19 19:14:31 +0100

    updates

--- restaurant.js
+++ restaurant.js
@@ -2,45 +2,12 @@
 https://striveschool.herokuapp.com/api/reservation/" */
 
 
-// Allow DOM to be fully charged 
-/* window.onload = async () => {
-    await loadReservations()
-}*/
+document.addEventListener("DOMContentLoaded", getReservations);
+document.addEventListener("DOMContentLoaded", putReservation);
+document.addEventListener("DOMContentLoaded", deleteReservation);
 
-document.addEventListener("DOMContentLoaded", loadReservations);
-document.addEventListener("DOMContentLoaded", sendReservation);
-document.addEventListener("DOMContentLoaded", Delete);
-
-//GET Function to obtain the list of the reservations
-// Old Functions
-/*async function loadReservations() {
-    let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/")
-    let resObj = await reservations.json()
-
-    let list = document.querySelector("#reservations")
-    list.innerHTML += resObj.map(x => `<li>${x.name} -> ${x.numberOfPersons} </li>`).join("")
-} */
-
-// New Function
-/*async function loadReservations() {
-    let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/")
-    let resObj = await reservations.json()
-
-    let list = document.querySelector("#reservations")
-    list.innerHTML += resObj.map(x =>
-          `<tr>
-             <td>${x.name}</td>
-             <td>${x.phone}</td>
-             <td>${x.numberOfPersons}</td>
-             <td>${x.smoking}</td>
-             <td>${x.specialRequests}</td>
-             <td>${x._id}</td>
-           </tr>`).join("")
-    }
-*/
-
-// New Function 2
-async function loadReservations() {
+// GET Function
+async function getReservations() {
     let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/")
     let resObj = await reservations.json()
 
@@ -56,7 +23,7 @@ async function loadReservations() {
     }
 
 // POST Function to obtain the list of the reservations
-async function sendReservation() {
+async function putReservation() {
     let myReservation = {
         name: document.querySelector("#name").value,
         phone: document.querySelector("#phone").value,
@@ -66,7 +33,7 @@ async function sendReservation() {
         specialRequests: document.querySelector("#extra").value,
     }
 
-    let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/", {
+    let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/",{
         method: "POST",
         headers: {
             "content-type": "application/json"
@@ -74,15 +41,17 @@ async function sendReservation() {
         body: JSON.stringify(myReservation)
     })
 
-  // await loadReservations()
+   await getReservations()
 }
 
 //DELETE Function to DELETE a post
-function Delete(){
+function deleteReservation(){
     document.querySelector("#cancelbook").addEventListener("click", async function() {
         const id = document.querySelector("#idbook").value;
         let reservations = await fetch(`https://striveschool.herokuapp.com/api/reservation/${id}`,{
-            method: "DELETE"
+            method: "DELETE",
         })
+        document.querySelector(".greenText").style.backgroundColor = "green"
+        document.querySelector("#success").innerHTML = "Reservation Cancelled!"
     });
 }
