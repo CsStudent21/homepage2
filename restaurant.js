d8694b470ed4c56782d671617836acf03b2d6bb7
CsStudent21 <tpadula@hey.com>
2021-03-24 15:46:40 +0100

    check

--- restaurant.js
+++ restaurant.js
@@ -1,16 +1,17 @@
 /* The source of my API 
-https://striveschool.herokuapp.com/api/reservation/" */
+https://striveschool.herokuapp.com/api/reservation/ */
 
 
+// CALL the Functions
 document.addEventListener("DOMContentLoaded", getReservations);
-document.addEventListener("DOMContentLoaded", putReservation);
+document.addEventListener("DOMContentLoaded", postReservation);
 document.addEventListener("DOMContentLoaded", deleteReservation);
 
 // GET Function
 async function getReservations(){
     let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/")
     let reservationsJson = await reservations.json()
-    document.querySelector("#reservations").innerHTML = reservationsJson.map(x => `<tr>
+    document.querySelector("#reservationsBook").innerHTML = reservationsJson.map(x => `<tr>
     <td>${x.name}</td>
     <td>${x.phone}</td>
     <td>${x.numberOfPersons}</td>
@@ -18,58 +19,54 @@ async function getReservations(){
     <td>${x.specialRequests}</td>
     <td>${x._id}</td>
   </tr>`).join("")
-}
+};
+
+// Function Color
+
+function Color(){
+    let colorTextForm = document.querySelectorAll(".form-control");
+    for(let i = 0; i < colorTextForm.length; i++){
+        colorTextForm[i].style.backgroundColor = "limegreen";
+    }
+};
 
 // POST Function to obtain the list of the reservations
-async function putReservation() {
-    let myReservation = {
-        name: document.querySelector("#name").value,
-        phone: document.querySelector("#phone").value,
-        numberOfPersons: document.querySelector("#people").value,
-        smoking: document.querySelector("#smoker").checked,
-        dateTime: document.querySelector("#date").value,
-        specialRequests: document.querySelector("#extra").value,
-    }
 
-    let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/",{
-        method: "POST",
-        headers: new Headers ({
-            "Content-Type": "application/json",
-        }),
-        body: JSON.stringify(myReservation),
-    })
+function postReservation(){
+    document.querySelector("#postReservation").addEventListener("click", async function() {
+        let myReservation = {
+            name:document.querySelector("#name").value,
+            phone:document.querySelector("#phone").value,
+            numberOfPersons:document.querySelector("#numberOfPersons").value,
+            smoking:document.querySelector("#smoking").checked,
+            dateTime:document.querySelector("#dateTime").value,
+            specialRequests:document.querySelector("#specialRequests").value,
+        }
+
+        await fetch("https://striveschool.herokuapp.com/api/reservation/", {
+            method: "POST",
+            headers: new Headers ({
+                "Content-Type": "application/json",
+            }),
+            body: JSON.stringify(myReservation),
+        })
+
+        Color();
+        document.querySelector("#postSuccess").innerHTML = "Table Reserved!"
+    });
 };
-
-/* OLD POST Function
-async function putReservation() {
-    let myReservation = {
-        name: document.querySelector("#name").value,
-        phone: document.querySelector("#phone").value,
-        numberOfPersons: document.querySelector("#numberOfPersons").value,
-        smoking: document.querySelector("#smoking").checked,
-        dateTime: document.querySelector("#dateTime").value,
-        specialRequests: document.querySelector("#specialRequests").value,
-    }
-
-    let reservations = await fetch("https://striveschool.herokuapp.com/api/reservation/", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(myReservation),
-    })
-
-};
-*/
 
 //DELETE Function to DELETE a post
 function deleteReservation(){
     document.querySelector("#cancelbook").addEventListener("click", async function() {
-        const id = document.querySelector("#idbook").value;
-        let reservations = await fetch(`https://striveschool.herokuapp.com/api/reservation/${id}`,{
+        const _id = document.querySelector("#idbook").value;
+        let reservations = await fetch(`https://striveschool.herokuapp.com/api/reservation/${_id}`,{
             method: "DELETE",
         })
         document.querySelector(".greenText").style.backgroundColor = "lightgreen"
-        document.querySelector("#success").innerHTML = "Reservation Cancelled!"
+        document.querySelector("#deleteSuccess").innerHTML = "Reservation Cancelled!"
     });
 };
+
+
+
