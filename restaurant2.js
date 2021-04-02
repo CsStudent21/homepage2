d05aa5ddcf426197a124143788169b4afb0a66b3
CsStudent21 <tpadula@hey.com>
2021-04-02 05:01:30 +0200

    mistery

--- restaurant2.js
+++ restaurant2.js
@@ -32,6 +32,44 @@ async function getReservations(){
 }; */
 
 // POST Function to make a new reservation
+function postReservation(){
+    // Define Variable of the New Reservation
+    let myReservation = {
+        name:document.querySelector("#name").value,
+        phone:document.querySelector("#phone").value,
+        numberOfPersons:document.querySelector("#numberOfPersons").value,
+        smoking:document.querySelector("#smoking").checked,
+        dateTime:document.querySelector("#dateTime").value,
+        specialRequests:document.querySelector("#specialRequests").value,
+    }
+
+    // POST Method for the New Reservation
+    fetch("https://striveschool.herokuapp.com/api/reservation/", {
+            method: "POST",
+            body: JSON.stringify(myReservation),
+            headers: {"Content-Type": "application/json",}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json));
+//END of the main Function
+/*
+// data to be sent to the POST request
+let _data = {
+  title: "foo",
+  body: "bar", 
+  userId:1
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: "POST",
+  body: JSON.stringify(_data),
+  headers: {"Content-type": "application/json; charset=UTF-8"}
+})
+.then(response => response.json()) 
+.then(json => console.log(json));
+.catch(err => console.log(err));*/
+};
+
 /*
 function postReservation(){
         let myReservation = {
